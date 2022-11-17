258630d93603fc73a426fb948c953ca14ef4d71d
biruk <birukhkebe1@gmail.com>
2022-11-17 14:23:59 -0600

    Word display fixed

--- app/src/main/java/com/example/spanishlanguagelearningapplication/VocabularyLesson.java
+++ app/src/main/java/com/example/spanishlanguagelearningapplication/VocabularyLesson.java
@@ -8,7 +8,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
+class vars
+{
+    public static int i =0 ;
+    //public
+}
 public class VocabularyLesson extends AppCompatActivity {
 
     @Override
@@ -33,35 +37,41 @@ public class VocabularyLesson extends AppCompatActivity {
         spanishList.add("tres");
         spanishList.add("cuatro");
 
-        int i =0 ;
+
 
         TextView englishText = (TextView) findViewById(R.id.englishWord);
-        englishText.setText(englishList.get(i));
+        //englishText.setText(englishList.get(vars.i));
 
         TextView spanishText =(TextView) findViewById(R.id.spanishWord);
-        spanishText.setText(spanishList.get(i));
+        //spanishText.setText(spanishList.get(vars.i));
 
         //When next is clicked
         Button next =  findViewById(R.id.nextButton);
         Button previous = findViewById(R.id.previousButton);
 
+        //while(vars.i < englishList.size()) {
 
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //for(int j=1; j< englishList.size();j++) {
-                    englishText.setText(englishList.get(i+1));
-                    spanishText.setText(spanishList.get(i+1));
-                //}
-             }
-        });
+            next.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    
+
+                    englishText.setText(englishList.get(vars.i));
+                    spanishText.setText(spanishList.get(vars.i));
+                    vars.i++;
+
+                }
+            });
+
+        //}
 
         previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                englishText.setText(englishList.get(0));
-                spanishText.setText(spanishList.get(0));
+                vars.i--;
+                englishText.setText(englishList.get(vars.i));
+                spanishText.setText(spanishList.get(vars.i));
             }
         });
 
